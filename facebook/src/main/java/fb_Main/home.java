6a5604c70ba0ac4d59e633cfe62c271b8b8062d5
shivj <shivj@192.168.0.112>
2022-11-04 16:45:03 +0530

    new 1sdt branch

---
+++ facebook/src/main/java/fb_Main/home.java
@@ -0,0 +1,28 @@
+package fb_Main;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class home {
+	@FindBy(xpath="//div[@aria-label=\"More options\"]")private WebElement navigate_to_log;
+	@FindBy(xpath="//span[text()='Log Out']")private WebElement logout;
+	@FindBy(xpath="//span[text()='Log Out']")private WebElement final_logout;
+
+	
+	public home(WebDriver driver) {
+		PageFactory.initElements(driver, this);}
+	public void logout_navigate() throws InterruptedException {
+		navigate_to_log.click();
+		Thread.sleep(1000);
+			}
+	
+	public void logout() {
+logout.click();
+	}
+	public void final_logouts() {
+		final_logout.click();
+		}
+
+}
